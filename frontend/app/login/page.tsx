211320ac9d27f05010163f1a355a9061fd007250
Camilo <camilovettori@gmail.com>
2026-04-04 00:19:31 +0100

    jjjjj

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -61,8 +61,8 @@ export default function LoginPage() {
               <Image
                 src="/log.png"
                 alt="Ziffera logo"
-                width={100}
-                height={100}
+                width={70}
+                height={70}
                 priority
                 className="object-contain"
               />
